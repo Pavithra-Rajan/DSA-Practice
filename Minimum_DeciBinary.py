fd3f27a2d73b4a42213435f039ef92e901450acf
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-06-08 19:19:35 +0400

    added minimum decibinary

---
+++ Minimum_DeciBinary.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """
+        num_str=[x for x in str(n)]
+        return max(num_str)
