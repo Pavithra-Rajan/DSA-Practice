4bfa0039ca62dbbf944203403e830ad3478eea99
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-06-13 03:23:15 +0400

    added number of students with no lunch

---
+++ No_Students_Lunch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        count=0
+        while count<len(students):
+            if students[0]==sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
+                count=0
+                # print(students)
+            else:
+                
+                students.append(students.pop(0))
+                count+=1
+                # print(students)
+        return count
