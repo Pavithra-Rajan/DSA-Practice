77c3c3a9905bf2f039588fdc04c42498e4a80dbc
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-06-16 03:31:44 +0400

    added elem 25 occurrence

---
+++ Elem_25_Occur.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n=len(arr)//4
+        for i in range(len(arr)-n):
+            if arr[i]==arr[i-1] and i:
+                continue
+            if arr[i]==arr[i+n]:
+                return arr[i]
+        
+        
