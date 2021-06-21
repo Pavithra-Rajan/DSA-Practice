8dbf01b5f076931bada7bad57fe3cce728217221
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-06-21 21:46:29 +0400

    added valid parentheses 2.0

---
+++ Valid_Parentheses2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        if s=="*" :
+            return True
+        if len(s)==1:
+            return False
+        
+        left=0
+        for i in s:
+            if i == ')':
+                left-=1
+            else:
+                left+=1
+            if left<0:
+                return False
+        if left==0:
+            return True
+        
+        right=0
+        for i in reversed(s):
+            if i == '(':
+                right-=1
+            else:
+                right+=1
+            if right<0:
+                return False
+        return True
