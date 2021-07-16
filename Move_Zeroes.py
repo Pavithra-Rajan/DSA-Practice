9259ea963df212ea6fe543ade5c45c3a2b4b3764
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-07-17 03:23:27 +0400

    added move zeroes to end

---
+++ Move_Zeroes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l,r=0,len(nums)-1
+        while l<r:
+            if nums[l]==0:
+                nums.append(nums.pop(l))
+                r-=1
+                continue
+            l+=1
+        return nums
