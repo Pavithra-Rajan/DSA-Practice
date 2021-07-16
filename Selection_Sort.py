55fec5deaf28f55ee63e92848b0fce99f1e0574c
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-07-17 02:58:34 +0400

    added selection sort,bubble,heap

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,3 +1,29 @@
+// Heap Sort
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def heapify(nums,n,i):
+            max_=i
+            l=2*i+1
+            r=2*i+2
+            if l<n and nums[max_]<nums[l]:
+                max_=l
+            if r<n and nums[max_]<nums[r]:
+                max_=r
+            if max_!=i:
+                nums[max_],nums[i]=nums[i],nums[max_]
+                heapify(nums,n,max_)
+        def heapsort(nums):
+            n=len(nums)
+            for i in range(n//2-1,-1,-1):
+                heapify(nums,n,i)
+        
+            for i in range(n-1,0,-1):
+                nums[i],nums[0]=nums[0],nums[i]
+                heapify(nums,i,0)
+            
+        heapsort(nums)
+        return nums
+
 //Bubble Sort
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
