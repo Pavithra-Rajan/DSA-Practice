7fa9341961b4407bf1f3a373e8f51ec26dc52226
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-07-16 23:03:39 +0400

    added selection sort,bubble

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,3 +1,15 @@
+//Bubble Sort
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def bubble(nums):
+            for i in range(len(nums)):
+                for j in range(0,len(nums)-i-1):
+                    if nums[j]>nums[j+1]:
+                        nums[j],nums[j+1]=nums[j+1],nums[j]
+        bubble(nums)
+        return(nums)
+
+// Selection Sort
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         def selection(nums):
