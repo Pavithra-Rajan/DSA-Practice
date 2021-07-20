5968da9a9afdb94564bc13c7f1984ee9b8bc3adc
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-07-20 21:00:55 +0400

    added next greater to right O(m+n

--- Next_greater.py
+++ Next_greater.py
@@ -1,6 +1,22 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        #O(m+n) algo
+        stack=[]
+        hashm={}
         ret=[]
+        for i in nums2:
+            while stack and i>stack[-1]:
+                hashm[stack.pop()]=i
+            stack.append(i)
+        while stack:
+            hashm[stack.pop()]=-1
+        for i in nums1:
+            ret.append(hashm[i])
+        return ret
+            
+            
+        # O(nm) algo
+        """ret=[]
         for i in nums1:
             ind=nums2.index(i)
             flag=0
@@ -12,5 +28,5 @@ class Solution:
             if flag==0:
                 ret.append(-1)
         
-        return ret
+        return ret"""
             
